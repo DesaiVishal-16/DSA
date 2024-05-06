a3c08a679d665c8d92a1ce8a8f3460cb55f2d4ec
VishalDesai <desaidesai62230@gmail.com>
2024-05-06 06:10:13 +0530

    Added pattern 13

--- basicDSA/patterns.js
+++ basicDSA/patterns.js
@@ -273,6 +273,7 @@ function binaryNumberTriangle(N){
     }
     return pattern
 }
+
 console.log(binaryNumberTriangle(6));
 
 
@@ -305,3 +306,31 @@ function numberCrownPatterns(N){
 
 
 console.log(numberCrownPatterns(6));
+
+
+
+
+//Pattern - 13: Increasing Number Triangle Pattern
+
+// 1
+// 2  3
+// 4  5  6
+// 7  8  9  10
+// 11  12  13  14  15
+// 16  17  18  19  20  21
+
+
+function increaseNumberTriangle(N){
+     pattern += ""
+    let num = 1 
+    for(i=1;i<=N;i++){
+        let row = ""
+      for(j=1;j<=i;j++){
+        row += num + " "
+        num=num+1    
+      }
+      pattern +=row.trim()+ '\n'
+    }
+    return pattern
+}
+console.log(increaseNumberTriangle(6));
