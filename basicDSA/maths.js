e5adc9fcbd2384541ee2a72a5e94defaef4a04dc
VishalDesai <desaidesai62230@gmail.com>
2024-05-12 09:49:25 +0530

    Added GCD, Divisors, Prime

--- basicDSA/maths.js
+++ basicDSA/maths.js
@@ -112,3 +112,96 @@ function armstrongNum(N){
       return sum===num
 }
 console.log(armstrongNum(153));
+
+
+
+// 5. Find GCD of two numbers
+
+// Problem Statement: Given two integers N1 and N2, find their greatest common divisor.
+
+// The Greatest Common Divisor of any two integers is the largest number that divides both integers.
+
+
+// Input:N1 = 9, N2 = 12
+                
+// Output:3
+// Explanation:Factors of 9: 1, 3 and 9
+// Factors of 12: 1, 2, 3, 4, 6, 12
+// Common Factors: 1, 3 out of which 3 is the greatest hence it is the GCD.
+
+
+function findGcd(N1,N2){
+    let gcd = 1
+      for(i=1;i<=Math.min(N1,N2);i++){
+        if(N1%i===0 && N2%i===0){
+              gcd = i
+        }
+      }
+       return gcd
+}
+console.log(findGcd(9,12))
+
+
+// 6. Print all Divisors of a given Number
+
+// Problem Statement: Given an integer N, return all divisors of N.
+
+// A divisor of an integer N is a positive integer that divides N without leaving a remainder.
+// In other words, if N is divisible by another integer without any remainder, then that integer is considered a divisor of N.
+
+
+// Input:N = 36
+// Output:[1, 2, 3, 4, 6, 9, 12, 18, 36]
+// Explanation: The divisors of 36 are 1, 2, 3, 4, 6, 9, 12, 18, 36.
+
+function findDivisors(N) {
+    let allDiv = []
+    for (let i = 1; i <= N; i++) {
+        if (N % i === 0) {
+            allDiv.push(i)
+        }
+    }
+    return allDiv;
+}
+console.log(findDivisors(36));
+
+
+
+
+// 7. Check if a number is prime or not
+
+//Problem Statement: Given an integer N, check whether it is prime or not. 
+//A prime number is a number that is only divisible by 1 and itself and the total number of divisors is 2.
+
+// Input:N = 2
+// Output:True
+// Explanation: 2 is a prime number because it has two divisors: 1 and 2 (the number itself).
+
+//TC => O(N)
+function checkPrime(N){
+    let init = 0 
+   for(let i=1 ;i <= N;i++){
+      if(N % i === 0){
+         init += 1
+      }
+    }
+     return init == 2 ? true : false
+}
+console.log(checkPrime(2));
+
+
+//Optimized 
+//TC => O(sqrt(N))
+function checkPrime(N){
+    if(N<=1) return false
+    if(N<=3) return true
+     
+    for(i=2;i<Math.sqrt(N);i++){
+        if(N%i==0){
+        return false
+        }  
+    }
+    return true
+}
+console.log(checkPrime(2));
+
