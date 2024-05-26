366f525a2d9c00ae3412cef33fd75406023477a9
VishalDesai <desaidesai62230@gmail.com>
2024-05-26 09:42:52 +0530

    Added Q3.Count frequency of each element in the array

--- basicDSA/hashing.js
+++ basicDSA/hashing.js
@@ -1,3 +1,4 @@
+// Q. 1
 //Given an array of integers: [1, 2, 1, 3, 2] and we are given some queries: [1, 3, 4, 2, 10]. For each query, 
 //we need to find out how many times the number appears in the array. For example, if the query is 1 our answer would be 2, 
 //and if the query is 4 the answer will be 0. 
@@ -23,7 +24,8 @@ arr.forEach((num)=>{
 const result = queries.map(query=>hashMap[query]||0)
 
 // Print results
-console.log("Mehod 1");
+console.log("1. Mehod 1");
+console.log(result);
 result.forEach((res,index)=>{
  console.log(`The number ${queries[index]} appears ${res} times in the array.`);
 })
@@ -48,13 +50,16 @@ arr2.forEach(num => {
 const results2 = queries2.map(query => (query < maxArraySize ? frequencyArray[query] : 0));
 
 // Print results
-console.log("Mehod 2");
+console.log("1. Mehod 2");
+console.log(results2);
 results2.forEach((result, index) => {
   console.log(`The number ${queries2[index]} appears ${result} times in the array.`);
 });
 
 
 
+
+//Q. 2
 //Given the string: “abcdabefc” we are given some queries: [a, c, z]. 
 //For each query, we need to find out how many times the character appears in the string.
 //For example, if the query is ‘a’ our answer would be 2, and if the query is ‘z’ the answer will be 0. 
@@ -77,7 +82,46 @@ const resultStr = queriesStr.map(query => {
     return charCode < maxArraySizeStr ? frequencyArrayStr[charCode] : 0
 });
 
-console.log("Alphabets:");
+console.log("2. Alphabets:");
+console.log(resultStr);
 resultStr.forEach((res, index) => { 
     console.log(`The character '${queriesStr[index]}' appears ${res} times in the array.`);
 });
+
+
+
+// Q. 3
+
+// Count frequency of each element in the array
+
+// Problem statement: Given an array, we have found the number of occurrences of each element in the array.
+
+// Examples:
+
+// Example 1:
+// Input: arr[] = {10,5,10,15,10,5};
+// Output: 10=>3
+// 	        5=>2
+//         15=>1
+// Explanation: 10 occurs 3 times in the array
+// 	             5 occurs 2 times in the array
+//              15 occurs 1 time in the array
+
+
+const arr3 = [10,5,10,15,10,5]
+
+
+const frequencyArray3 = {}
+
+arr3.forEach((num)=>{
+  if(frequencyArray3[num]!==undefined){
+     frequencyArray3[num]++
+    }else{
+      frequencyArray3[num] = 1
+    }
+})
+console.log("3. Count frequency of each element in the array");
+for(const [key,value] of Object.entries(frequencyArray3)){
+  console.log(`${key}=>${value}`);
+}
+
