167e4f3279cfb4f883b2ed0662367c5feff1bbf4
VishalDesai <desaidesai62230@gmail.com>
2024-07-20 19:24:21 +0530

    "Added Second Largest element"

--- Array/Easy/secondLargest.js
+++ Array/Easy/secondLargest.js
@@ -1,35 +1,35 @@
-//Find Second Largest Element in an array
+//find second largest element in an array
 
-//Problem Statement: Given an array, find second largest element in the array. Print ‘-1’ in the event that either of them doesn’t exist.
+//problem statement: given an array, find second largest element in the array. print ‘-1’ in the event that either of them doesn’t exist.
 
-// Example 1:
+// example 1:
 
-// Input:
+// input:
 //  [1,2,4,7,7,5]
-// Output:
-// 	Second Largest : 5
-// Explanation:
-//  The elements are as follows 1,2,3,5,7,7 and hence second largest of these is 5.
+// output:
+// 	second largest : 5
+// explanation:
+//  the elements are as follows 1,2,3,5,7,7 and hence second largest of these is 5.
 
 function secondLargestEl(arr) {
   if (arr.length < 2) return -1;
 
-  let largest = arr[0]; //or -Infinity =>To handle negative numbers.
-  let sLargest = -1;
+  let largest = arr[0]; //or -infinity =>to handle negative numbers.
+  let slargest = -1;
 
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] > largest) {
-      sLargest = largest;
+      slargest = largest;
       largest = arr[i];
-    } else if (arr[i] < largest && arr[i] > sLargest) {
-      sLargest = arr[i];
+    } else if (arr[i] < largest && arr[i] > slargest) {
+      slargest = arr[i];
     }
   }
-  return sLargest === -Infinity ? -1 : sLargest;
+  return slargest === -infinity ? -1 : slargest;
 }
 let arr = [1, 2, 4, 7, 7, 5];
-const secondLaeget = secondLargestEl(arr);
-console.log(secondLaeget);
+const secondLargest = secondLargestEl(arr);
+console.log(secondLargest);
 
-//TC => O(N)
-//SC => O(1)
+//tc => o(n)
+//sc => o(1)
---
+++ Array/Easy/secondSmallest.js
@@ -0,0 +1,35 @@
+//find second smallest element in an array
+
+//problem statement: given an array, find second smallest element in the array. print ‘-1’ in the event that either of them doesn’t exist.
+
+// example 1:
+
+// input:
+//  [1,2,4,7,7,5]
+// output:
+// 	second largest : 2
+// explanation:
+//  the elements are as follows 1,2,3,5,7,7 and hence second smallest of these is 2.
+
+function secondSmallestEl(arr) {
+  if (arr.length < 2) return -1;
+
+  let smallest = Infinity;
+  let sSmallest = -Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < smallest) {
+      sSmallest = smallest;
+      smallest = arr[i];
+    } else if (arr[i] < sSmallest && arr[i] !== sSmallest) {
+      sSmallest = arr[i];
+    }
+  }
+  return sSmallest;
+}
+let arr = [1, 2, 4, 7, 7, 5];
+const secondSmallest = secondSmallestEl(arr);
+console.log(secondSmallest);
+
+//tc => o(n)
+//sc => o(1)
